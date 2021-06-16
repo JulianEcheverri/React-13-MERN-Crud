268ae49c52e6f2017357c95733a73f1602569440
JulianEcheverri <julianecheverri@outlook.com>
2021-06-16 14:07:01 -0500

    Modifying types comments

--- mern-tasks-client/src/types/index.js
+++ mern-tasks-client/src/types/index.js
@@ -1,10 +1,12 @@
 // Types
 // Actions
-// For describing current project's behavior
 
+// For project
 export const FORM_PROJECT = 'FORM_PROJECT';
 export const GET_PROJECTS = 'GET_PROJECTS';
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const FORM_ERROR = 'FORM_ERROR';
 export const CURRENT_PROJECT = 'CURRENT_PROJECT';
 export const DELETE_PROJECT = 'DELETE_PROJECT';
+
+// For tasks
