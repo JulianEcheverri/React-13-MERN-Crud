1d4895244cab0667ac67cad6261ddc7db05341c4
JulianEcheverri <julianecheverri@outlook.com>
2021-06-20 21:37:57 -0500

    Setting MongoDB connection with Node server

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv') .config({path: 'variables.env'});
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        });
+        console.log('Connection successful');
+    }
+    catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+module.exports = connectToDB;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log("Desde index.js");
+const express = require('express');
+const connectToDB = require('./config/db');
+
+// Create server
+const app = express();
+
+// Database connection
+connectToDB();
+
+// Assign the port
+const PORT = process.env.PORT || 4000;
+
+// Set routes
+app.get('/', (req, res) => {
+    res.send('It works');
+});
+
+// Run the server
+app.listen(PORT, () => {
+    console.log(`Server runing at port: ${PORT}`);
+});
